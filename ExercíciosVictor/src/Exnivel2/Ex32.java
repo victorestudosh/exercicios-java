8cc73f4bbed86ec743bce7529035dba675dc4ef9
victor.estagiario <victor.estagiario@192.168.99.153>
2026-07-22 13:42:48 -0300

    Dois Exercicios novos nivel 2

--- ExercíciosVictor/src/Exnivel2/Ex32.java
+++ ExercíciosVictor/src/Exnivel2/Ex32.java
@@ -1,7 +1,13 @@
 package Exnivel2;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 public class Ex32 {
 	public static void main(String[] args) {
+		Random aleatorio = new Random();
+		JOptionPane.showMessageDialog(null, "Gerar número Aleatório", "Ex32", JOptionPane.INFORMATION_MESSAGE, null);
 		
-	}
+		JOptionPane.showMessageDialog(null, "(" + aleatorio.nextInt(11) + "), esse número foi gerado aleatóriamente com a Classe Random", "Classe Random", JOptionPane.INFORMATION_MESSAGE, null);
+	} 
 }
---
+++ ExercíciosVictor/src/Exnivel2/Ex33.java
@@ -0,0 +1,15 @@
+package Exnivel2;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Ex33 {
+	public static void main(String[] args) {
+		Random aleatorio = new Random(6);
+		int[] array = new int[101];
+		JOptionPane.showMessageDialog(null, "Simular lançamento de dado 100 vezes", "Ex33", JOptionPane.INFORMATION_MESSAGE, null);
+		
+		
+	}
+}
